8a54625c833fa51c39791f22be8f6e149110867b
CleverChuk <chubiyke@gmail.com>
2022-11-27 07:15:45 -0500

    MIP-79 - add coprocessor opcodes

--- mips/src/main/java/com/cleverchuk/mips/simulator/fpu/FpuOpcode.java
+++ mips/src/main/java/com/cleverchuk/mips/simulator/fpu/FpuOpcode.java
@@ -4,7 +4,7 @@ import androidx.annotation.NonNull;
 import java.util.Arrays;
 import java.util.Set;
 import java.util.stream.Collectors;
-
+@SuppressWarnings("all")
 public enum FpuOpcode {
     // Data transfer instructions
     LDC1("ldc1"),
@@ -16,7 +16,34 @@ public enum FpuOpcode {
     MFC1("mfc1"),
     MFHC1("mfhc1"),
     MTC1("mtc1"),
-    MTHC1("mthc1")
+    MTHC1("mthc1"),
+
+    // FPU IEEE arithmetic operations
+    ABS_S("abs.s"),
+    ABS_D("abs.d"),
+    ADD_S("add.s"),
+    ADD_D("add.d"),
+    CMP_COND_S("cmp.cond.s"),
+    CMP_COND_D("cmp.cond.d"),
+    DIV_S("div.s"),
+    DIV_D("div.d"),
+    MUL_S("mul.s"),
+    MUL_D("mul.d"),
+    NEG_S("neg.s"),
+    NEG_D("neg.d"),
+    SQRT_S("sqrt.s"),
+    SQRT_D("sqrt.d"),
+    SUB_S("sub.s"),
+    SUB_D("sub.d"),
+
+    // FPU non-IEEE-approximate Arithmetic Instructions
+    RECIP_S("recip.s"),
+    RECIP_D("recip.d"),
+    RSQRT_S("rsqrt.s"),
+    RSQRT_D("rsqrt.d"),
+
+    // FPU Fused Multiply-Accumulate Arithmetic Operations (Release 6)
+
 
     ;
     public static Set<String> FPU_OPCODES = Arrays.stream(FpuOpcode.values()).map(FpuOpcode::getValue)
